dc71f766974124feca0388a6eb12a9942bc58d40
Jouni Kiviperä <jounikivipera@gmail.com>
2025-03-31 13:55:34 +0300

    Korjattu virheellinen koodi tehtävää 8j varten

--- Workout.js
+++ Workout.js
@@ -3,9 +3,7 @@ import { logger } from './logger.js';
 
 
 const createWorkout = username => {
-    // if (typeof username !== 'string' || username.length < 4)
-    if (typeof username === 'number')  // tarkoituksella väärin
-    {
+    if (typeof username !== 'string' || username.length < 4){
         throw new Error("the parameter username is not valid");
     }
     return db('workouts').insert({ username });
